197887db4468bf30f808c0ac979cc4049ced94ad
Alondrx <alondrx07@gmail.com>
2025-11-29 15:52:04 -0600

    feat: logic and interface design

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const path = require("path");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
+
+let win;
 
 function createWindow() {
-    const win = new BrowserWindow({ 
-        width: 800, 
-        height: 600, 
-        webPreferences: { nodeIntegration: true, contextIsolation: false } });
-    win.loadFile(path.join(__dirname, 'index.html'));
-    win.setMenuBarVisibility(false);
+  win = new BrowserWindow({
+    width: 700,
+    height: 550,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+
+  });
+
+  win.removeMenu();
+  win.loadFile("index.html");
+  ipcMain.on("load-page", (event, page) => {
+    win.loadFile(page);
+  });  
 }
 
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
+  });
+
+
+  ipcMain.handle("take-screenshot", async () => {
+    const image = await win.webContents.capturePage(); // Capture the window content
+    const screenshotPath = path.join(app.getPath("desktop"), `screenshot-${Date.now()}.png`);
+    fs.writeFileSync(screenshotPath, image.toPNG());
+    return screenshotPath;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
